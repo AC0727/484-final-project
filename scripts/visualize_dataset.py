0f4be047839c71a85b17426b64ff4d8f9725c9fd
AC0727 <adele.chen@ashbury.ca>
2026-04-14 15:26:31 -0400

    add up to model instantiation

--- scripts/visualize_dataset.py
+++ scripts/visualize_dataset.py
@@ -1,19 +1,6 @@
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import torch
-
 from mylibs.datasets import VOCCatBinaryDataset
 from mylibs.transforms import get_image_transform
-
-def unnormalize(img_tensor):
-    """
-    img_tensor: Tensor [3, 224, 224]
-    returns: Tensor [3, 224, 224] in range [0, 1] for display
-    """
-    mean = torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
-    std = torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
-    img_tensor = img_tensor * std + mean
-    return torch.clamp(img_tensor, 0, 1)
+from mylibs.utils import unnormalize, visualize_cat_images
 
 def main():
     dataset = VOCCatBinaryDataset(
@@ -24,36 +11,7 @@ def main():
         download=False
     )
 
-    shown = 0
-    i = 0
-
-    while shown < 3 and i < len(dataset):
-        image, label, target = dataset[i]
-
-        if label.item() == 1.0:
-            image = unnormalize(image)
-            image_np = image.permute(1, 2, 0).numpy()   # [224, 224, 3]
-
-            fig, ax = plt.subplots(1, figsize=(6, 6))
-            ax.imshow(image_np)
-
-            for box in target["boxes"]:
-                xmin, ymin, xmax, ymax = box.tolist()
-                rect = patches.Rectangle(
-                    (xmin, ymin),
-                    xmax - xmin,
-                    ymax - ymin,
-                    linewidth=2,
-                    edgecolor="r",
-                    facecolor="none"
-                )
-                ax.add_patch(rect)
-
-            ax.set_title(f'label={int(label.item())}, image_id={target["image_id"]}')
-            plt.show()
-            shown += 1
-
-        i += 1
+    visualize_cat_images(dataset, 3)
 
 if __name__ == "__main__":
     main()
